dfb485a5a699cdcf8fc765c714ffb424a51d3f0b
Daniel <dddanielliu@gmail.com>
2026-06-30 11:22:35 +0800

    context use type assertion
    - a context always has to be created outside the component
    - whereas its future value will always be set inside a component
    - so there is that gap that needs to be plugged in
    - and type assertion is how to do it.

--- frontend/src/components/context/User.tsx
+++ frontend/src/components/context/User.tsx
@@ -4,24 +4,20 @@ import { UserContext } from "./UserContext"
 export const User = () => {
     const userContext = useContext(UserContext)
     const handleLogin = () => {
-        if(userContext){
-            userContext.setUser({
-                name: 'Vishwas',
-                email: 'vishwas@example.com'
-            })
-        }
+        userContext.setUser({
+            name: 'Vishwas',
+            email: 'vishwas@example.com'
+        })
     }
     const handleLogout = () => {
-        if (userContext){
-            userContext.setUser(null);
-        }
+        userContext.setUser(null);
     }
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
             <button onClick={handleLogout}>Logout</button>
-            <div>User name is {userContext?.user?.name}</div>
-            <div>User email is {userContext?.user?.email}</div>
+            <div>User name is {userContext.user?.name}</div>
+            <div>User email is {userContext.user?.email}</div>
         </div>
     )
 }
